d0924a3903c388155ab226be162bf8a65a3e8166
smile <understandable997@gmail.com>
2024-01-04 10:44:31 +0800

    create project

---
+++ app/src/main/java/com/wzp/androidknowledge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wzp.androidknowledge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
